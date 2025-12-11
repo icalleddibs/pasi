eef37bab2aaa2d4221b70c0dd9107929cfe73e54
Diba Alam <diba.alam@mail.utoronto.ca>
2025-12-10 22:03:38 -0500

    colour country and regions

---
+++ lib/data/country_data.dart
@@ -0,0 +1,238 @@
+// add only countries that have been visited along with visit dates
+
+final Map<String, dynamic> countryData = {
+  // NORTH AMERICA ---------------------------------------------------------------------------------------------------------------------------------
+  // canada
+  'ca': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': ['Ontario', 'Quebec', 'Alberta', 'British Columbia', 'Saskatchewan', 'Manitoba', 'Yukon'],
+  },
+  // united states
+  'us': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': ['Alaska', 'California', 'Hawaii', 'New York', 'Washington', 'Maryland', 'Florida', 'Massachusetts', 'Philadelphia', 'New Jersey', 'South Dakota', 'North Dakota', 'Illinois', 'Nevada', 'Arizona', 'Georgia', 'Michigan'],
+  },
+  // mexico
+  'mx': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // CARIBBEAN ---------------------------------------------------------------------------------------------------------------------------------
+  // dominica 
+  'dm': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // dominican republic
+  'do': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // grenada
+  'gd': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // bonaire, sint eustatius and saba
+  'bq': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // sint maarten
+  'sx': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // st kitts and nevis
+  'kn': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // anguilla
+  'ai': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // antigua and barbuda
+  'ag': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // st. lucia
+  'lc': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // trinidad and tobago
+  'tt': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // british virgin islands
+  'vg': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // st. vincent and the grenadines
+  'vc': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // cayman islands
+  'ky': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // haiti
+  'ht': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // barbados
+  'bb': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // bahamas
+  'bs': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // aruba
+  'aw': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // jamaica
+  'jm': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // CENTRAL AMERICA ---------------------------------------------------------------------------------------------------------------------------------
+  // belize
+  'bz': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // honduras
+  'hn': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // EUROPE ---------------------------------------------------------------------------------------------------------------------------------
+  // greece
+  'gr': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // italy
+  'it': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // albania
+  'al': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // croatia
+  'hr': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // england
+  'gb': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': ['England'],
+  },
+  // france
+  'fr': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': ['Île-de-France', 'Provence-Alpes-Côte d\'Azur'],
+  },
+  // spain
+  'es': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+  // portugal
+  'pt': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': ['Lisbon', 'Porto'],
+  },
+  // the netherlands
+  'nl': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': ['North Holland', 'South Holland', 'Utrecht'],
+  },
+  // germany
+  'de': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': ['Cologne'],
+  },
+  // belgium
+  'be': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': ['Brussels-Capital Region'],
+  },
+  // ASIA ---------------------------------------------------------------------------------------------------------------------------------
+  // japan
+  'jp': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': ['Tokyo', 'Kyoto', 'Osaka', 'Hokkaido', 'Hiroshima', 'Nara', 'Nagano'],
+  },
+  // south korea
+  'kr': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': ['Seoul'],
+  },
+  // MIDDLE EAST ---------------------------------------------------------------------------------------------------------------------------------
+  // united arab emirates
+  'ae': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': ['Dubai', 'Abu Dhabi'],
+  },
+  // oman
+  'om': {
+    'visited': true,
+    'dates': [],
+    'visitedStates': [],
+  },
+};
--- lib/screens/screen_map.dart
+++ lib/screens/screen_map.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'supported_countries_map.dart';
 import 'screen_passport.dart';
 import 'screen_profile.dart';
+
+// Example usage of countries_world_map package
+// https://pub.dev/packages/countries_world_map/example
+// https://examples.simplewidgets.dev/#/countries_world_map
 
 class MapScreen extends StatelessWidget {
   const MapScreen({super.key});
@@ -9,11 +14,13 @@ class MapScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('World Map')),
-      body: const Center(
-        child: Text(
-          'Interactive map goes here',
-          style: TextStyle(fontSize: 18),
-        ),
+      body: Center(
+        child: SupportedCountriesMap(),
+        //child: SimpleMap(
+          //instructions: SMapWorld.instructions,   // <- THIS is the world map
+          //defaultColor: Colors.grey.shade300,     // <- default color
+
+        //),
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: 1,
