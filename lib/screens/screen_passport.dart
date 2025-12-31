bcd83af92258256c18815336102f357336f05fb3
Diba Alam <diba.alam@mail.utoronto.ca>
2025-12-31 13:30:58 -0500

    added flip buttons and stamp rotation

--- lib/screens/screen_passport.dart
+++ lib/screens/screen_passport.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:turnable_page/turnable_page.dart';
+import 'dart:math';
 import 'screen_map.dart';
+import 'screen_profile.dart';
 import 'package:pasi/data/country_data.dart';
 
 // Next Steps:
@@ -10,6 +12,7 @@ import 'package:pasi/data/country_data.dart';
   // - add the dates visited in country_data.dart for testing
 // - start with a closed book to begin the homepage (and flip back to closed)
   // - possibly a passport page with ID and profile
+  
 
 class PassportPageData {
   final String countryCode; // 'ca', 'us'
@@ -72,6 +75,7 @@ class PassportPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final random = Random();
     return LayoutBuilder(
       builder: (context, constraints) {
         final double pageWidth = constraints.maxWidth;
@@ -86,16 +90,20 @@ class PassportPage extends StatelessWidget {
             int index = row * 2 + col;
             // here i will edit so that i can make a cover page always, or start with closed book maybe
             if (index < stamps.length) {
+            final angle = (random.nextDouble() * 30 - 15) * pi / 180;
               rowChildren.add(
                 SizedBox(
                   width: stampWidth,
                   height: stampHeight,
                   child: Padding(
                     padding: EdgeInsets.all(spacing / 2),
-                    child: Image.asset(
-                      'assets/stamps/stamp_${stamps[index].countryCode}.png', // make sure all stamps are using this naming convention
-                      fit: BoxFit.contain,
-                    ),
+                    child: Transform.rotate(
+                      angle: angle, // slight random rotation
+                      child: Image.asset(
+                        'assets/stamps/stamp_${stamps[index].countryCode}.png', // make sure all stamps are using this naming convention
+                        fit: BoxFit.contain,
+                      ),
+                    )
                   ),
                 ),
               );
@@ -138,12 +146,15 @@ class _PassportScreenState extends State<PassportScreen> {
   int currentIndex = 0;
   late final List<PassportPageData> allStamps;
   late final List<List<PassportPageData>> pagesPerPage;
+  late PageFlipController _controller;
+
 
   @override
   void initState() {
     super.initState();
     allStamps = buildPassportPages();
     pagesPerPage = chunkPages(allStamps, 6); // 6 stamps per page because of the 3x2 layout
+    _controller = PageFlipController();
   }
 
   @override
@@ -152,21 +163,57 @@ class _PassportScreenState extends State<PassportScreen> {
       appBar: AppBar(
         title: const Text('Passport'),
       ),
-      body: Center(
-        child: AspectRatio(
-          aspectRatio: 792 / 512, // this is the book shape but not pages, need to adjust figma background size to the page
-          child: TurnablePage(
-            pageCount: pagesPerPage.length,
-            autoResponseSize: true,
-            settings: FlipSettings(
-              flippingTime: 700,
+      body: Row(
+        children: [
+          // left button
+          Padding(
+            padding: const EdgeInsets.only(left: 8),
+            child: IconButton(
+              iconSize: 40,
+              icon: const Icon(Icons.chevron_left),
+              onPressed: () {
+                _controller.previousPage();
+              },
             ),
-            builder: (context, pageIndex, constraints) {
-              return PassportPage(stamps: pagesPerPage[pageIndex]);
-            },
           ),
-        ),
+
+          // book in the middle
+          Expanded(
+            child: Center(
+              child: AspectRatio(
+                aspectRatio: 792 / 512,
+                child: TurnablePage(
+                  controller: _controller,
+                  pageCount: pagesPerPage.length,
+                  autoResponseSize: true,
+                  settings: FlipSettings(
+                    flippingTime: 700,
+                    showCover: true,
+                  ),
+                  builder: (context, pageIndex, constraints) {
+                    return PassportPage(
+                      stamps: pagesPerPage[pageIndex],
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+
+          // right button
+          Padding(
+            padding: const EdgeInsets.only(right: 8),
+            child: IconButton(
+              iconSize: 40,
+              icon: const Icon(Icons.chevron_right),
+              onPressed: () {
+                _controller.nextPage();
+              },
+            ),
+          ),
+        ],
       ),
+      
 
       // navigation bar, consistent across screens
       bottomNavigationBar: BottomNavigationBar(
@@ -194,6 +241,11 @@ class _PassportScreenState extends State<PassportScreen> {
               MaterialPageRoute(
                 builder: (_) => const MapScreen(),
               ),
+            );
+          } else if (index == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const ProfileScreen()),
             );
           }
         },
